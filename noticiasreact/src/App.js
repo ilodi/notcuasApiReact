c3a753b345fd32262d3f0242a272ad7a0797ac80
David Lopez <lodi@MacBook-Pro-de-David.local>
2019-06-25 22:58:40 -0500

    fetch

--- noticiasreact/src/App.js
+++ noticiasreact/src/App.js
@@ -2,10 +2,21 @@ import React, { Component } from 'react';
 
 class App extends Component {
   state = {}
+
+  async componentDidMount() {
+    const url = `https://newsapi.org/v2/top-headlines?country=mx&category=business&apiKey=4c286d9d2ebd402897fa358d403e863a`;
+
+    //fetch perdir datos mediante un json
+    const respuesta = await fetch(url);
+    const noticias = await respuesta.json();
+    console.log(noticias);
+  }
+
+
   render() {
-    return(
+    return (
       <>
-      <h1>ss</h1>
+        <h1>ss</h1>
       </>
     );
   }
