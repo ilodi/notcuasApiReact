50efe0d21704c3adb0766cc965ba6bdf09fef6d4
David Lopez <lodi@MacBook-Pro-de-David.local>
2019-06-27 23:34:37 -0500

    api

--- noticiasreact/src/components/Noticia.js
+++ noticiasreact/src/components/Noticia.js
@@ -3,14 +3,21 @@ import React from 'react';
 const Noticia = ({ noticia }) => {
 	//codigo js es mejor aquí
 	//Extraer Datos mediante distrocchorin
-	const { urlToImage, url, title, description, sourece } = noticia;
-	//
+	const { urlToImage, url, title, description, source } = noticia;
+	//Condicionalemnte cargar la imagen si esta disponible
+
+	const imagen = {urlToImage} ? 
+		<div className="card-image">
+			<img src={urlToImage} alt={title}/>
+			<span className="card-title">
+				{source.name}
+			</span>
+		</div>
+	: null;
 	return (
 		<div className="col s12 m6 l4">
 			<div className="card">
-				<div className="card-image">
-					<img src={urlToImage} alt="{title}" />
-				</div>
+				{imagen}
 				<div className="card-content">
 					<h3>{title}</h3>
 					<p>
