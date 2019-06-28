827c2e90fd26d8345e343c6013a750d93313974a
David Lopez <lodi@MacBook-Pro-de-David.local>
2019-06-27 23:02:27 -0500

    d

--- noticiasreact/src/components/Noticia.js
+++ noticiasreact/src/components/Noticia.js
@@ -1,11 +1,30 @@
 import React from 'react';
 
-const Noticia = () =>{
-    return(
-        <h1>
-            Desde noticias
-        </h1>
-    );
+const Noticia = ({ noticia }) => {
+	//codigo js es mejor aquí
+	//Extraer Datos mediante distrocchorin
+	const { urlToImage, url, title, description, sourece } = noticia;
+	//
+	return (
+		<div className="col s12 m6 l4">
+			<div className="card">
+				<div className="card-image">
+					<img src={urlToImage} alt="{title}" />
+				</div>
+				<div className="card-content">
+					<h3>{title}</h3>
+					<p>
+						{description}
+					</p>
+				</div>
+				<div className="card-action"> 
+					<a href={url} target="_black" rel="noopener noreferrer" className="waves-effect waves-light btn" >
+						Ver Más
+					</a>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Noticia;
